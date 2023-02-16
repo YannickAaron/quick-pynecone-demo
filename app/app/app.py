48aa0b6935b21d2d0c9756c0912e372f9e895399
Yannick <33485945+YannickAaron@users.noreply.github.com>
2023-02-16 09:17:15 +0100

    added database test support

--- app/app/app.py
+++ app/app/app.py
@@ -4,15 +4,29 @@ from pcconfig import config
 
 import pynecone as pc
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
+
+class Message(pc.Model, table=True):
+    username: str
+    message: str
+    timestamp: str
 
 
 class State(pc.State):
     count: int = 0
 
+    messages: list[Message] = []
+
     def pressed(self):
         self.count += 1
+
+    def add_message(self):
+        with pc.session() as session:
+            session.add(Message(username="Test2", message="Test3", timestamp="Test4"))
+            self.messages = session.query(Message).all()
+
+    def get_messages(self):
+        with pc.session() as session:
+            self.messages = session.query(Message).all()
 
 
 def index() -> pc.Component:
@@ -30,8 +44,34 @@ def index() -> pc.Component:
                     "color": "rgb(107,99,246)",
                 },
             ),
-            spacing="1.5em",
-            font_size="2em",
+            pc.button(
+                "Get messages",
+                border="0.1em solid",
+                padding="0.5em",
+                border_radius="0.5em",
+                on_click=State.get_messages,
+                _hover={
+                    "color": "rgb(107,99,246)",
+                },
+            ),
+            pc.button(
+                "Add message",
+                border="0.1em solid",
+                padding="0.5em",
+                border_radius="0.5em",
+                on_click=State.add_message,
+                _hover={
+                    "color": "rgb(107,99,246)",
+                },
+            ),
+            pc.foreach(
+                State.messages,
+                lambda message: pc.vstack(
+                    pc.text(message.username, font_size="1.5em"),
+                    pc.text(message.message, font_size="1.5em"),
+                    pc.text(message.timestamp, font_size="1.5em"),
+                ),
+            ),
         ),
         padding_top="10%",
     )
